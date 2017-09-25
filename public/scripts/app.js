2d13a3f9dd8120e0dcd62991a20700aac8183aa9
partyzant <partyzant>
2017-09-25 22:53:55 +0200

    first commit

---
+++ public/scripts/app.js
@@ -0,0 +1,12 @@
+"use strict";
+
+console.log('app i runing');
+var template = React.createElement(
+  "h1",
+  null,
+  "Tis is jfff"
+);
+
+var appRoot = document.getElementById("app");
+
+ReactDOM.render(template, appRoot);
---
+++ src/app.js
@@ -0,0 +1,6 @@
+console.log('app i runing');
+var template = <h1>Tis is jfff</h1>;
+
+var appRoot = document.getElementById("app");
+
+ReactDOM.render(template, appRoot);
